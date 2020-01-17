bf1f4c4bebc8acc486fcc5f587054beacb0bd626
michoutoure <michoutoure@yahoo.fr>
2020-01-17 13:02:52 +0100

    Initial commit

---
+++ src/main/java/Exercices/Rectangle.java
@@ -0,0 +1,22 @@
+package Exercices;
+
+
+    public class Rectangle {
+        public double Longueur;
+        public double Largeur;
+
+        public Rectangle(double L, double l){
+            this.Longueur=L;
+            this.Largeur=l;
+        }
+
+        public Rectangle(double v, int i, int i1) {
+        }
+
+        public double surface(){
+            return this.Longueur*this.Largeur;
+        }
+        public void afficher(){
+            System.out.println("La surface du rectangle est : " + this.surface());
+        }
+    }
---
+++ src/test/java/testRectangle.java
@@ -0,0 +1,10 @@
+package exercices;
+
+import Exercices.Rectangle;
+
+public class testRectangle {
+    public static void main(String [] args) {
+        Rectangle monRectangle = new Rectangle(4.0, 5.0);
+        monRectangle.afficher();
+    }
+}
